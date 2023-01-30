8a73bd3a484eeeb28469207be59744f938a6ac9a
RomanMelnikow <119843583+RomanMelnikow@users.noreply.github.com>
2023-01-30 17:28:19 +0300

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,15 @@
+import math
+print('Hello World')
+print('hello world!')
+a=input('Введите +  -  / * ')
+A=int(input('Введите А '))
+B=int(input('Введите B '))
+
+if a=='+':
+	print('Ответ',A+B)
+elif a=='-':
+	print('Ответ',A-B)
+elif a=='*':
+	print('Ответ',A*B)
+elif a=='/':
+	print('Ответ',A/B)
